3c5add9ed9ad3dab386e0bf1e6978ceef5d5b89f
kenji <kenjix12x22@gamil.com>
2021-01-14 05:51:15 +0000

    フォームのデザイン修正

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -7,6 +7,7 @@ class LoginController < ApplicationController
     user = User.find_by(email: params[:login][:email].downcase)
     if user&&user.authenticate(params[:login][:password])
       log_in user
+      params[:login][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -15,7 +16,7 @@ class LoginController < ApplicationController
   end
   
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to login_path
   end
   
