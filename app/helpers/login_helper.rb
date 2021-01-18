456ef151aa052b6072882ae57004531ffe0483ac
kenji <kenjix12x22@gamil.com>
2021-01-18 05:47:40 +0000

    タイムライン修正

--- app/helpers/login_helper.rb
+++ app/helpers/login_helper.rb
@@ -44,5 +44,10 @@ module LoginHelper
         redirect_to login_url
       end
    end
+   
+   def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'users#index'
   get    '/login',   to: 'login#new'
   post   '/login',   to: 'login#create'
   delete '/logout',  to: 'login#destroy'
