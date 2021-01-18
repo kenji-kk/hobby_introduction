bea0e781ca6f55634c3d77b97693be1336eec7a0
kenji <kenjix12x22@gamil.com>
2021-01-18 14:51:45 +0000

    フォロワー表示ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:show,]
+   before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   
   def index
@@ -32,6 +33,20 @@ class UsersController < ApplicationController
   
   def manyusers
     @users = User.paginate(page: params[:page])
+  end
+  
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+  
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
   
  private
